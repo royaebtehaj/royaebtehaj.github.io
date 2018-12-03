0be117353d654944d17b1ff64b3784009e50ee9c
royaebtehaj <royaebtehaj@users.noreply.github.com>
2018-12-03 09:38:30 -0800

    dd

--- js/sketch.js
+++ js/sketch.js
@@ -1,129 +1,26 @@
 /*
-* @name Drawing
-* @description Generative painting program.
-*/
-
-// All the paths
-var paths = [];
-// Are we painting?
-var painting = false;
-// How long until the next circle
-var next = 0;
-// Where are we now and where were we?
-var current;
-var previous;
-
+ * @name Mixture
+ * @frame 710,400 (optional)
+ * @description Display a box with three different kinds of lights.
+ */
 function setup() {
-  createCanvas(720, 400);
-  current = createVector(0,0);
-  previous = createVector(0,0);
-};
+	createCanvas(710, 400, WEBGL);
+	noStroke();
+}
 
 function draw() {
-  background(200);
-  
-  // If it's time for a new point
-  if (millis() > next && painting) {
+	background(0);
 
-    // Grab mouse position      
-    current.x = mouseX;
-    current.y = mouseY;
+	// Orange point light on the right
+	pointLight(150, 100, 0, 500, 0, 200);
 
-    // New particle's force is based on mouse movement
-    var force = p5.Vector.sub(current, previous);
-    force.mult(0.05);
+	// Blue directional light from the left
+	directionalLight(0, 102, 255, -1, 0, 0);
 
-    // Add new particle
-    paths[paths.length - 1].add(current, force);
-    
-    // Schedule next circle
-    next = millis() + random(100);
+	// Yellow spotlight from the front
+	pointLight(255, 255, 109, 0, 0, 300);
 
-    // Store mouse values
-    previous.x = current.x;
-    previous.y = current.y;
-  }
-
-  // Draw all paths
-  for( var i = 0; i < paths.length; i++) {
-    paths[i].update();
-    paths[i].display();
-  }
-}
-
-// Start it up
-function mousePressed() {
-  next = 0;
-  painting = true;
-  previous.x = mouseX;
-  previous.y = mouseY;
-  paths.push(new Path());
-}
-
-// Stop
-function mouseReleased() {
-  painting = false;
-}
-
-// A Path is a list of particles
-function Path() {
-  this.particles = [];
-  this.hue = random(100);
-}
-
-Path.prototype.add = function(position, force) {
-  // Add a new particle with a position, force, and hue
-  this.particles.push(new Particle(position, force, this.hue));
-}
-
-// Display plath
-Path.prototype.update = function() {  
-  for (var i = 0; i < this.particles.length; i++) {
-    this.particles[i].update();
-  }
-}  
-
-// Display plath
-Path.prototype.display = function() {
-  
-  // Loop through backwards
-  for (var i = this.particles.length - 1; i >= 0; i--) {
-    // If we shold remove it
-    if (this.particles[i].lifespan <= 0) {
-      this.particles.splice(i, 1);
-    // Otherwise, display it
-    } else {
-      this.particles[i].display(this.particles[i+1]);
-    }
-  }
-
-}  
-
-// Particles along the path
-function Particle(position, force, hue) {
-  this.position = createVector(position.x, position.y);
-  this.velocity = createVector(force.x, force.y);
-  this.drag = 0.95;
-  this.lifespan = 255;
-}
-
-Particle.prototype.update = function() {
-  // Move it
-  this.position.add(this.velocity);
-  // Slow it down
-  this.velocity.mult(this.drag);
-  // Fade it out
-  this.lifespan--;
-}
-
-// Draw particle and connect it with a line
-// Draw a line to another
-Particle.prototype.display = function(other) {
-  stroke(0, this.lifespan);
-  fill(0, this.lifespan/2);    
-  ellipse(this.position.x,this.position.y, 8, 8);    
-  // If we need to draw a line
-  if (other) {
-    line(this.position.x, this.position.y, other.position.x, other.position.y);
-  }
+	rotateY(map(mouseX, 0, width, 0, PI));
+	rotateX(map(mouseY, 0, height, 0, PI));
+	box(200);
 }
